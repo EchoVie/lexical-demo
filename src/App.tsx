7590ea3636fa5db2967bcf3fc81e6efac1a07561
18720551903 <Echo981022@163.com>
2024-06-16 17:24:05 +0800

    feat:demo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import RichTextEditor from "./components/RichTextEditor";
+
+function App() {
+
+  return (
+    <div style={{margin: "0 auto"}}>
+      <RichTextEditor />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/RichTextEditor/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+    // $getRoot,
+    // $getSelection,
+    EditorState,
+} from 'lexical';
+import {LexicalComposer} from '@lexical/react/LexicalComposer';
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {HorizontalRulePlugin} from '@lexical/react/LexicalHorizontalRulePlugin';
+import {ListPlugin} from '@lexical/react/LexicalListPlugin';
+import {CheckListPlugin} from '@lexical/react/LexicalCheckListPlugin';
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import Toolbar from "../../components/Toolbar";
+import LocalStoragePlugin from "../../plugins/LocalStoragePlugin";
+// import TreeViewPlugin from '../../plugins/TreeViewPlugin';
+import Stack from "@mui/material/Stack";
+import Placeholder from "../Placeholder";
+import CodeHighlightPlugin from '../../plugins/CodeHighlightPlugin';
+import {initialConfig} from "../../constants";
+import useStyles from "./styles";
+
+const handleChange = (editorState: EditorState) => {
+    editorState.read(() => {
+        // const root = $getRoot();
+        // const selection = $getSelection();
+    });
+}
+
+export const RichTextEditor = () => {
+    // we retrieved the content from local storage in the Editor component
+    const content = localStorage.getItem(initialConfig.namespace);
+
+    const classes = useStyles();
+
+    return (
+        <LexicalComposer
+            initialConfig={{...initialConfig, editorState: content}}
+        >
+            <Stack className={classes.container}>
+                <Toolbar editable/>
+                <RichTextPlugin
+                    contentEditable={<ContentEditable className={classes.contentEditable}/>}
+                    placeholder={<Placeholder className={classes.placeholder}>Enter some rich text...</Placeholder>}
+                    ErrorBoundary={LexicalErrorBoundary}
+                />
+                <CodeHighlightPlugin />
+                <OnChangePlugin onChange={handleChange}/>
+                <HistoryPlugin/>
+                <LocalStoragePlugin namespace={initialConfig.namespace}/>
+                <ListPlugin/>
+                <HorizontalRulePlugin />
+                <CheckListPlugin/>
+            </Stack>
+            {/* {false && <TreeViewPlugin />} */}
+        </LexicalComposer>
+    )
+}
+
+export default RichTextEditor;
